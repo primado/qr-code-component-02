0b3a8010b3ae95dc997526dfce544ce7f7545623
primado <primadoprincepa@gmail.com>
2022-12-29 13:51:16 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      light_grey: "hsl(212, 45%, 89%)",
+      greyish_blue: "hsl(220, 15%, 55%)",
+      dark_blue: "hsl(218, 44%, 22%)",
+      sky_500: "#6366f1",
+      rose: "#be123c",
+
+    }
+  },
+  plugins: [],
+}
